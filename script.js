64cde09b3543d2a06a498709f014fffa7eaa2d9e
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-20 20:08:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
-  // write your code here
+   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let radnomStr = ""
+    for(let i = 1; i <= l; i++){
+      let randomIndex = parseInt(Math.random()*str.length)
+      radnomStr += str[randomIndex]
+    }
+    return radnomStr
+	
 }
 
 // Do not change the code below.
